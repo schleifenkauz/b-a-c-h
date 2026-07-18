b1da6791310061afbbb7fccbab0434d80df15b23
Nikolaus Knop <niko.knop003@gmail.com>
2026-07-18 16:36:45 +0200

    Improve layout, slow down planets

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 let planet_layer, main_layer, interactive_layer, text_layer;
 let natural, flat, kapelle;
+let canvases;
 
 function makeCanvas(w, h, z_index) {
     const func = (p) => {
@@ -16,21 +17,25 @@ function setup() {
     natural = loadImage("res/natural.png");
     flat = loadImage("res/flat.png");
     kapelle = loadImage("res/sixtinische-kapelle.png");
-    
+
     const W = windowWidth;
-    const H = 1000; 
+    const H = 950;
 
     createCanvas(W, H).parent("menu");
     main_layer = this;
-    planet_layer = makeCanvas(W, H, 1);    
+    planet_layer = makeCanvas(W, H, 1);
     text_layer = makeCanvas(W, H, 2);
     interactive_layer = makeCanvas(W, H, 3);
-    text_layer.background(255, 255, 255, 200);
+    text_layer.background(255, 255, 255, 150);
     text_layer.noStroke();
     planet_layer.noStroke();
-    [this, main_layer, interactive_layer, planet_layer, text_layer].forEach((layer) => {
+    canvases = [this, interactive_layer, planet_layer, text_layer]
+    canvases.forEach((layer) => {
         layer.textFont("EB Garamond")
-        layer.textSize(28);  
+        layer.textSize(28);
+        const scale = windowWidth / W;
+        console.log("Scale", scale);
+        //layer.canvas.style.transform = `scale(${1})`;
     })
 }
 
@@ -50,13 +55,11 @@ function interpolate(samples, t) {
     }
 }
 
-async function loadHTML(file, elementId) {
-  const response = await fetch(file);
-  const html = await response.text();
-  document.getElementById(elementId).innerHTML = html;
+async function loadPage(file, elementId='content') {
+    const response = await fetch(file);
+    const html = await response.text();
+    document.getElementById(elementId).innerHTML = html;
 }
-
-loadHTML("header.html", "content");
 
 const ORIGIN_Y = 500;
 
@@ -95,20 +98,22 @@ let current = new Set([{
 }]);
 
 let planets = new Set([]);
-let last_spawn_time = 0; 
-const spawn_interval = 8000;
-const planet_speed = 0.5;
+let last_spawn_time = 0;
+const spawn_interval = 15000;
+const planet_speed = 0.1;
 const notehead_size = 12;
 const notehead_rotation = -PI / 8;
 const planet_spawn_time = 300;
 add_planet([256, 256, 128]);
 
 const spawn_angles = [
-    { angle: 2.75 * PI, rotate: 0, name: "Concert lectures", offset_y: 1, color: [255, 0, 0], url: "pages/concerts.html" }, //Concert Lectures: 
-    { angle: 3.25 * PI, rotate: PI / 2, name: "Unterricht", offset_y: -1, color: [0, 255, 0], url: "pages/unterricht.html" }, //Unterpunkte: Coaching, Instrumentalunterricht 
-    { angle: 3.75 * PI, rotate: PI, name: "Lehre", offset_y: -1, color: [0, 0, 255], url: "pages/lehre.html" }, //Vorlesungen, Seminare
-    { angle: 4.25 * PI, rotate: -PI / 2, name: "Fundament", offset_y: 1, color: [0, 255, 255], url: "pages/fundament.html" } //
-]
+    { angle: 2.75 * PI, rotate: 0, name: "Concert Lectures", offset_y: 1, color: [255, 0, 0], url: "pages/concerts.html" }, //Concert Lectures: 
+    { angle: 3.25 * PI, rotate: PI / 2, name: "Lebensunterricht", offset_y: -1, color: [0, 255, 0], url: "pages/unterricht.html" }, //Unterpunkte: Coaching, Instrumentalunterricht 
+    { angle: 3.75 * PI, rotate: PI, name: "Akademische Lehre", offset_y: -1, color: [0, 0, 255], url: "pages/lehre.html" }, //Vorlesungen, Seminare
+    { angle: 4.25 * PI, rotate: -PI / 2, name: "Think Tank", offset_y: 1, color: [0, 255, 255], url: "pages/think-tank.html" } //
+];
+
+//Stichwörter einblenden, wenn hover
 
 const clef_dx = points[points.length - 1].x - points[0].x;
 const clef_dy = points[points.length - 1].y - points[0].y;
@@ -119,17 +124,17 @@ let links = [];
 
 const BLACK = 50;
 
-const LINK_RADIUS = 270;
+const LINK_RADIUS = 240;
 
 function accent(opacity = 100) {
     return color([20, 100, 0, opacity]);
 }
 
 const BACH = [
-    {angle: PI, y: -14, text: "A", text_offset: 50}, 
-    {angle: 2*PI, y: -42, accidental: null, text: "Complete", text_offset: -100}, 
-    {angle: 3*PI, y: -28, accidental: null, accidental: () => flat, scale: 0.12, offset_y: -9, offset_x: -7, text: "Be", text_offset: 0, color: 'green'}, 
-    {angle: 4*PI, y: -28, accidental: () => natural, scale: 0.05, offset_y: 0, offset_x: -12, text: "Human", text_offset: -25, color: 'pink'}
+    { angle: PI, y: -14, text: "A", text_offset: -10, textStyle: "italic", fontFamily: "serif" },
+    { angle: 2 * PI, y: -42, accidental: null, text: "Complete", text_offset: 55 },
+    { angle: 3 * PI, y: -28, accidental: null, accidental: () => flat, scale: 0.12, offset_y: -9, offset_x: -7, text: "BE", text_offset: -20, color: 'green' },
+    { angle: 4 * PI, y: -28, accidental: () => natural, scale: 0.05, offset_y: 0, offset_x: -12, text: "Human", text_offset: 50, color: 'pink' }
 ];
 
 function dist(cx, cy, x, y) {
@@ -139,16 +144,19 @@ function dist(cx, cy, x, y) {
 }
 
 function mouseMoved() {
+    if (!interactive_layer) return;
     interactive_layer.clear();
     links.forEach((link) => {
         const d = dist(link.x, link.y, mouseX, mouseY);
         let text_color, bg_color;
-        if (d < LINK_RADIUS / 2) {
-            interactive_layer.stroke(BLACK, BLACK, BLACK);
-            for (let i = -3; i <= 1; i++) {
-                const y = link.y + i * 16;
-                interactive_layer.stroke(150);
-                interactive_layer.line(link.x - 80, y, link.x + 80, y);
+        if (d < link.radius / 2) {
+            if (link.name != "Fundament") {
+                interactive_layer.stroke(BLACK);
+                for (let i = -3; i <= 1; i++) {
+                    const y = link.y + i * 16;
+                    interactive_layer.stroke(150);
+                    interactive_layer.line(link.x - 80, y, link.x + 80, y);
+                }
             }
             text_color = BLACK;
             bg_color = accent(40);
@@ -159,8 +167,12 @@ function mouseMoved() {
 
         //interactive_layer.fill(color(link.color.concat([50])));
         interactive_layer.fill(color(bg_color));
-        interactive_layer.stroke(accent());
-        interactive_layer.ellipse(link.x, link.y, LINK_RADIUS, LINK_RADIUS);
+        if (!link.current) {
+            interactive_layer.stroke(accent());
+        } else {
+            interactive_layer.stroke(BLACK);
+        }
+        interactive_layer.ellipse(link.x, link.y - 10, link.radius, link.radius);
 
         interactive_layer.fill(text_color);
         interactive_layer.noStroke();
@@ -173,13 +185,30 @@ function mouseMoved() {
 function mousePressed() {
     links.forEach((link) => {
         const d = dist(link.x, link.y, mouseX, mouseY);
-        if (d < LINK_RADIUS / 2) {
-            loadHTML(link.url, 'content');
+        if (d < link.radius / 2) {
+            openPage(link);   
         }
     })
 }
 
+async function openPage(link) {
+    await loadPage(link.url);
+    links.forEach(link => link.current = false);
+    link.current = true;
+    window.scrollBy({top: 500, behavior: "smooth"})
+    mouseMoved();
+} 
+
 let background_drawn = false;
+
+function loadWelcome() {
+    loadPage("pages/welcome.html");
+    links.forEach(link => link.current = false);
+    mouseMoved();
+}
+
+loadWelcome();
+
 
 function draw() {
     if (!background_drawn && kapelle.width > 1) {
@@ -199,25 +228,21 @@ function draw() {
             main_layer.line(0, y, width, y);
         }
     }
-
+    const content = document.getElementById("content");
+    content.style.opacity = clamp(t / 1000, 0, 1);
+    
     if (t >= planet_spawn_time) {
         current.forEach((o) => {
             main_layer.push();
             main_layer.translate(width / 2 + o.start_x, o.start_y + ORIGIN_Y);
             if (o.depth == 1 && o.text_drawn == false) {
-                const layer = interactive_layer;
-                const x = layer.width / 2 + o.start_x - layer.textWidth(o.name) / 2;
-                const y = ORIGIN_Y + o.start_y;
-                layer.stroke(accent());
-                layer.fill(accent());
-                layer.ellipse(width / 2 + o.start_x, ORIGIN_Y + o.start_y, LINK_RADIUS, LINK_RADIUS);
-                layer.noStroke();
-                layer.textSize(30);
-                layer.fill('white');
-                layer.text(o.name, x, y + o.offset_y * MENU_OFFSET);
+                links.push({
+                    current: false, name: o.name, url: o.url,
+                    x: width / 2 + o.start_x, y: o.start_y + ORIGIN_Y, offset_y: o.offset_y,
+                    color: o.highlight_color, radius: LINK_RADIUS
+                });
                 o.text_drawn = true;
-                console.log(o.color);
-                links.push({name: o.name, url: o.url, x: width / 2 + o.start_x, y: o.start_y + ORIGIN_Y, offset_y: o.offset_y, color: o.highlight_color, url: o.url})
+                mouseMoved();
             }
             //rotate(o.rotate);
             if (COLORFUL) {
@@ -226,7 +251,16 @@ function draw() {
                 o.saturation = clamp(o.saturation + rand2(10) + 0.05, 100, 255);
             } else {
                 main_layer.stroke(o.color, o.color, o.color, o.opacity);
-                o.color = clamp(o.color + rand2(20), 0, 150);
+                o.color = clamp(o.color + rand2(30), 0, 150);
+            }
+
+            if (o.angle > PI && links.length == 0) {
+                links.push({
+                    current: false, name: "Fundament", url: "pages/fundament.html",
+                    x: width / 2, y: ORIGIN_Y - 5, offset_y: 0,
+                    color: 'black', radius: 270
+                });
+                mouseMoved();
             }
 
             if (o.i < points.length && o.opacity > 0) {
@@ -283,73 +317,92 @@ function draw() {
                         }
                         text_layer.fill(accent(255));
                         text_layer.textSize(40);
-                        text_layer.text(a.text, -text_layer.textWidth(a.text) / 2 + a.text_offset, -90 - a.y);
+                        let x = -text_layer.textWidth(a.text) / 2 + a.text_offset;
+                        if (a.textStyle) {
+                            text_layer.textStyle(a.textStyle)
+                        } else {
+                            text_layer.textStyle('bold');
+                        }
+                        if (a.fontFamily) {
+                            text_layer.textFont(a.fontFamily);
+                            console.log(a.fontFamily);
+                        } else {
+                            text_layer.textFont('EB Garamond');
+                        }
+                        text_layer.text(a.text[0], x, -95 - a.y);
+                        x += text_layer.textWidth(a.text[0]);
+                        text_layer.textStyle('normal');
+                        if (a.text == "BE") {
+                            text_layer.textSize(25);
+                        }
+                        text_layer.text(a.text.slice(1), x, -95 - a.y);
 
                         text_layer.rotate(-PI / 8)
                         text_layer.fill(10);
                         text_layer.ellipse(0, 0, 30, 30 * 0.75);
                         text_layer.pop();
                     }
-                });
+                }); //living concerts, concertified lectures
             } else {
                 current.delete(o);
             }
             main_layer.pop();
         })
     }
-     
+
     planet_layer.clear();
     //planet_layer.text(frame_rate.toFixed(1) + " FPS", width / 2, 200)
 
-    planets.forEach((o) => {
+    planets.forEach((p) => {
         planet_layer.push();
-        planet_layer.translate(width / 2 + o.start_x, o.start_y + ORIGIN_Y);
-        if (o.i < 0) {
-            const p = (o.i + planet_spawn_time) / planet_spawn_time;
-            o.rotation = notehead_rotation * p;
-            o.size = p * notehead_size;
-            o.squish = 1 - (p * 0.25);
-            o.i += global_speed;
-        } else if (o.i < points.length) {
-            o.x = (points[o.i].x - points[0].x) * o.scale; 
-            o.y = (points[o.i].y - points[0].y) * o.scale;
-            o.i += global_speed;
-        } else if (o.radius < MAX_RADIUS * (o.scale ** 1.4)) {
-            o.rotate += PI / 48;
-            o.angle += d_angle * planet_speed * global_speed;
-            o.radius += d_radius * planet_speed * global_speed / ((o.radius + 1) ** o.radius_pow);
+        planet_layer.translate(width / 2 + p.start_x, p.start_y + ORIGIN_Y);
+        if (p.i < 0) {
+            const prog = (p.i + planet_spawn_time) / planet_spawn_time;
+            p.rotation = notehead_rotation * prog;
+            p.size = prog * notehead_size;
+            p.squish = 1 - (prog * 0.25);
+            p.i = Math.round(p.i + global_speed);
+        } else if (p.i < points.length) {
+            p.x = (points[p.i].x - points[0].x) * p.scale;
+            p.y = (points[p.i].y - points[0].y) * p.scale;
+            p.i = Math.round(p.i + global_speed * planet_speed);
+        } else if (p.radius < MAX_RADIUS * (p.scale ** 1.4)) {
+            p.rotate += PI / 48;
+            p.angle += d_angle * planet_speed * global_speed;
+            p.radius += d_radius * planet_speed * global_speed / ((p.radius + 1) ** p.radius_pow);
+            p.opacity -= 0.35 * planet_speed * global_speed * p.scale; 
             //o.step -= 0.003;
-            o.x = o.radius * cos(o.angle) + (clef_dx * o.scale);
-            o.y = o.radius * sin(o.angle) + (clef_dy * o.scale) - o.initial_radius;  
+            p.x = p.radius * cos(p.angle) + (clef_dx * p.scale);
+            p.y = p.radius * sin(p.angle) + (clef_dy * p.scale) - p.initial_radius;
             spawn_angles.forEach((spawn_angle) => {
-                if (Math.abs(spawn_angle.angle - o.angle) <= d_angle * planet_speed * global_speed / 2 && o.scale > (0.5 ** DEPTH) && Math.random() < 0.25) {
+                if (Math.abs(spawn_angle.angle - p.angle) <= d_angle * planet_speed * global_speed / 2 && p.scale > (0.5 ** DEPTH) && Math.random() < 0.5) {
                     //console.log("spawning");
                     planets.add({
-                        start_x: o.x + o.start_x, start_y: o.start_y + o.y,
-                        initial_radius: o.initial_radius / 2,
-                        x: 0, y: 0, size: o.size / 1.4, rotation: notehead_rotation, squish: 0.75,
-                        angle: PI / 2, radius: o.initial_radius / 2, i: 0,
-                        radius_pow: o.radius_pow + 0.25, scale: o.scale / 2,
-                        rotate: o.rotate + spawn_angle.rotate, color: o.color
+                        start_x: p.x + p.start_x, start_y: p.start_y + p.y,
+                        initial_radius: p.initial_radius / 2,
+                        x: 0, y: 0, size: p.size / 1.4, rotation: notehead_rotation, squish: 0.75,
+                        angle: PI / 2, radius: p.initial_radius / 2, i: 0,
+                        radius_pow: p.radius_pow + 0.25, scale: p.scale / 2,
+                        rotate: p.rotate + spawn_angle.rotate, color: p.color, opacity: p.opacity
                     })
                 }
-            })  
+            })
         } else {
-            planets.delete(o);
+            planets.delete(p);
         }
         if (COLORFUL) {
-            planet_layer.stroke(o.color[0], o.color[1], o.color[2]);
-            planet_layer.fill(o.color[0], o.color[1], o.color[2]);
+            planet_layer.stroke(p.color[0], p.color[1], p.color[2], p.opacity);
+            planet_layer.fill(p.color[0], p.color[1], p.color[2], p.opacity);
         } else {
-            planet_layer.stroke(0, 0, 0);
-            planet_layer.fill(0, 0, 0);
+            planet_layer.stroke(0, 0, 0, p.opacity);
+            planet_layer.fill(0, 0, 0, p.opacity);
         }
-        planet_layer.translate(o.x, o.y);
-        const stem_h = clamp(o.i / 10, 0, 33);
+        planet_layer.translate(p.x, p.y);
+        const stem_h = clamp(p.i / 10, 0, 33);
         //planet_layer.rotate(o.rotate);
-        planet_layer.line(o.size / 2, 0, o.size / 2, -stem_h * (o.size / notehead_size));
-        planet_layer.rotate(o.rotation);
-        planet_layer.ellipse(0, 0, o.size, o.size * o.squish);
+        planet_layer.line(p.size / 2, 0, p.size / 2, -stem_h * (p.size / notehead_size));
+        planet_layer.rotate(p.rotation);
+        planet_layer.ellipse(0, 0, p.size, p.size * p.squish);
         planet_layer.pop();
     });
 
@@ -367,7 +420,7 @@ function add_planet(color) {
     if (!color) {
         const hue = rand(50, 180);
         const saturation = rand(100, 255);
-        color = [hue + (255 - saturation), (255 - hue) + (255 - saturation), (255 - saturation)]; 
+        color = [hue + (255 - saturation), (255 - hue) + (255 - saturation), (255 - saturation)];
     }
     last_planet_color = color;
     last_spawn_time = Date.now();
@@ -375,7 +428,7 @@ function add_planet(color) {
         i: -planet_spawn_time, start_x: 0, start_y: 0, x: 0, y: 0,
         scale: 1, radius: 100, initial_radius: 100, angle: PI / 2,
         depth: 0, rotate: 0, squish: 1,
-        hue: 128, saturation: 128, step: 1, radius_pow: 0,
+        hue: 128, saturation: 128, step: 1, radius_pow: 0, opacity: 256,
         color: last_planet_color
     })
 }
@@ -390,10 +443,6 @@ function add_planet(color) {
 - am anfang: rosa, licht durch haut
 - urknall am anfang vor dem punkt
 
-- "Fundament" in der Mitte
-- letzte Spiralwindung in den Boden
-- Omega als Kuppel
-
 - 3-Menü Punkte + Basis 
 - scrollen => Menü wird kleiner, Inhalte erscheinen
 
@@ -407,5 +456,10 @@ function add_planet(color) {
 
 - f(x) = A => f'(x) = 0 -> omega
 
-- bassschlüssel - in den noteschlüssel zurück kehren. 
+- noten schnell ausfaden, langsamer
+- subtiler
+
+- fundament in der mitte in grau
+- 4 weitere punkte: einzelunterricht - kritiken - kontakt-formular, think tank - blog - instagram, akademische lehre - LinkedIn - vita, concert lectures - konzertkalender - YouTube - TikTok, Instagram 
+- irgendwas mathematisches
 */
